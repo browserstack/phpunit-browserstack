98155f64c6bbf1a8544f67ba60dfa454cf1f8dc7
hariharanbrowserstack <hariharan@browserstack.com>
2023-09-29 17:47:04 +0530

    Fix Win Issues

--- triggerTest.php
+++ triggerTest.php
@@ -1,7 +1,7 @@
 <?php
 // Check if an argument is provided for config_file
 $testFileArg = $argv[1] ?? 'Test.php';
-$testFile = "tests" . DIRECTORY_SEPARATOR . $var2;
+$testFile = "tests" . DIRECTORY_SEPARATOR . $testFileArg;
 putenv("TEST_FILE=$testFile");
 putenv('CONFIG_FILE=config/test.conf.json');
 // Execute the PHP script with the dynamically retrieved value
