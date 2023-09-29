6813b8ea0607c3489c0cc730bae97054fcbb152c
hariharanbrowserstack <hariharan@browserstack.com>
2023-09-29 20:16:39 +0530

    Fix Directory Separator

--- triggerTest.php
+++ triggerTest.php
@@ -2,8 +2,9 @@
 // Check if an argument is provided for config_file
 $testFileArg = $argv[1] ?? 'Test.php';
 $testFile = "tests" . DIRECTORY_SEPARATOR . $testFileArg;
+$configFile = "config" . DIRECTORY_SEPARATOR . "test.conf.json";
 putenv("TEST_FILE=$testFile");
-putenv('CONFIG_FILE=config/test.conf.json');
+putenv("CONFIG_FILE=$configFile");
 // Execute the PHP script with the dynamically retrieved value
 require 'lib/runner.php';
 
