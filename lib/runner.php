d26547f92f9c3d9621c14397f42a81bc35698de7
hariharanbrowserstack <hariharan@browserstack.com>
2023-10-03 22:42:38 +0530

    Enhancing Windows Tests on phpUnit - Sel-3

--- lib/runner.php
+++ lib/runner.php
@@ -13,8 +13,10 @@ if (!$test_file) {
 
 try {
     // the following code starts local binary, comment out if local testing not needed
-    $bs_local_args = array("key" => $GLOBALS['BROWSERSTACK_ACCESS_KEY']);
+    // Local binary started with additional args "logfile" => "logs.txt", "v" => true to get the local not started logs for debugging. Please remove them if not needed and cleanup logs if becoming huge.
+    $bs_local_args = array("key" => $GLOBALS['BROWSERSTACK_ACCESS_KEY'], "logfile" => "logs.txt", "v" => true);
     $bs_local = new BrowserStack\Local();
+    $currentOS = strtoupper(substr(PHP_OS, 0, 3));
     print("\nStarting Local Binary...\n");
     $bs_local->start($bs_local_args);
     if ($bs_local->isRunning()) {
@@ -22,6 +24,9 @@ try {
     }
     foreach ($CONFIG['capabilities'] as $key => $value) {
         $cmd = "TASK_ID=$key vendor/bin/phpunit $test_file 2>&1\n";
+        if ($currentOS == 'WIN') {
+            $cmd = "set TASK_ID=$key && vendor\bin\phpunit $test_file 2>&1\n";
+        }
         print_r($cmd);
     
         $procs[$key] = popen($cmd, "r");
@@ -34,6 +39,7 @@ try {
     }
 } catch (Exception $e) {
     echo $e->getMessage();
+    echo "Exception Trace:\n" . $e->getTraceAsString() . "\n";
 } finally {
     if($bs_local) {
         print("\nStopping local binary..\n");
